4a6f7054184697013935fb263e431f449fbb2d4d
Darryl Stevens <hwrdthdck@users.noreply.github.com>
2025-02-05 21:26:18 -0500

    Update generated_status.go

--- extension/oidcauthextension/internal/metadata/generated_status.go
+++ extension/oidcauthextension/internal/metadata/generated_status.go
@@ -8,7 +8,8 @@ import (
 
 var (
 	Type      = component.MustNewType("oidc")
-	ScopeName = "github.com/open-telemetry/opentelemetry-collector-contrib/extension/oidcauthextension"
+	//ScopeName = "github.com/open-telemetry/opentelemetry-collector-contrib/extension/oidcauthextension"
+	ScopeName = "github.com/hwrdthdck/opentelemetry-collector-contrib/extension/oidcauthextension"
 )
 
 const (
